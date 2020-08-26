e1ebbfe989e2d176878bdff881857f7b7412d374
Khoa Ma <khoama@gmail.com>
2020-08-25 23:20:39 -0700

    — adjust package config

--- services/web/src/app.tsx
+++ services/web/src/app.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { KeycloakProvider } from '@react-keycloak/web'
+import { KeycloakProvider } from '@react-keycloak/web';
 import keycloak from './auth';
 
-import './app.css';
-import DashboardController from './containers/dashboard'
+import 'app.css';
+import DashboardController from 'containers/dashboard';
 
 function App() {
   return (
     <KeycloakProvider keycloak={keycloak}>
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        H1st Workbench Planceholder
-        <DashboardController />
-      </header>
-    </div>
-    </KeycloakProvider> 
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          H1st Workbench Planceholder
+          <DashboardController />
+        </header>
+      </div>
+    </KeycloakProvider>
   );
 }
 
