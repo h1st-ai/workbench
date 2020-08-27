c8ab5ac42e72ffb69ac39de20242009f38a1872d
Khoa Ma <khoama@gmail.com>
2020-08-27 11:14:16 -0700

    — dashboard ui

--- services/web/src/components/app-header/index.tsx
+++ services/web/src/components/app-header/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withKeycloak } from '@react-keycloak/web';
 
 import Logo from './logo';
+import Icon from 'components/icon';
 import ProfilePhoto from 'components/profile-photo';
 import styles from './style.module.css';
 
-function AppHeader() {
+function AppHeader({ keycloak }: any) {
+  const [menustate, setMenuState] = useState(false);
   return (
     <header className={styles.appHeader}>
       <Logo />
-      <ProfilePhoto />
+      <span className={styles.title}>H1st projects</span>
+      <div className={styles.userMenu}>
+        <a onClick={() => setMenuState(!menustate)}>
+          <ProfilePhoto />
+          {renderMenu(menustate, keycloak)}
+        </a>
+      </div>
     </header>
   );
 }
 
-export default AppHeader;
+function renderMenu(menuOpenned: Boolean, keycloak: any) {
+  if (menuOpenned) {
+    return (
+      <ul>
+        {/* <li>
+          <a href={keycloak.createLogoutUrl()}>
+            <Icon icon="user" />
+            Account
+          </a>
+        </li>
+        <li>
+          <a href={keycloak.createLogoutUrl()}>
+            <Icon icon="gear" />
+            Settings
+          </a>
+        </li> */}
+        <li>
+          <a href={keycloak.createLogoutUrl()}>
+            <Icon icon="logout" />
+            Log out
+          </a>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default withKeycloak(AppHeader);
