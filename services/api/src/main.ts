183202bac62fb94a016967dd1d85c7f4da3bc7ae
Khoa Ma <khoama@gmail.com>
2020-08-28 00:21:01 -0700

    — add prefix for api

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -9,11 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { cors: true },
   );
 
-  if (process.env.NODE_ENV === 'dev') {
-    app.enableCors();
-  }
+  app.setGlobalPrefix('api');
 
   await app.listen(parseInt(process.env.API_PORT) || 3000, '0.0.0.0');
 }
