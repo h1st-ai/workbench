71131a797c8fd5f496c3e21919f47bd69c614597
Khoa Ma <khoama@gmail.com>
2020-08-27 19:57:00 -0700

    — clean up svg

--- services/web/src/components/icon/index.tsx
+++ services/web/src/components/icon/index.tsx
@@ -204,7 +204,7 @@ function Icon({
           fill={fill}
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
             clipRule="evenodd"
           />
