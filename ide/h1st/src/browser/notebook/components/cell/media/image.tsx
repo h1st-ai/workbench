edd2c7ef25791a124182a4d7bfa3955204058bad
Khoa Ma <khoama@gmail.com>
2020-09-26 21:27:03 -0700

    — memorize image renderings

--- ide/h1st/src/browser/notebook/components/cell/media/image.tsx
+++ ide/h1st/src/browser/notebook/components/cell/media/image.tsx
@@ -1,31 +1,61 @@
 import * as React from "react";
 
-const PNG = (props: any) => {
-  console.log("rendering png");
+export const MediaPNG = (props: any) => {
+  const ref = React.useRef<HTMLImageElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.src = `data:${props.mediatype};base64,${props.data}`;
+    }
+  }, [props.data]);
 
   return (
     <div className="cell-output-plot-background">
-      <img alt="" src={`data:${props.mediatype};base64,${props.data}`} />
+      <img ref={ref} alt="" />
     </div>
   );
 };
 
-PNG.defaultProps = {
+MediaPNG.defaultProps = {
   mediaType: "image/png",
 };
 
-export const MediaPNG = React.memo(PNG);
+export const MediaJPG = (props: any) => {
+  const ref = React.useRef<HTMLImageElement>(null);
 
-export const MediaJPG = (props: any) => (
-  <img alt="" src={`data:${props.mediatype};base64,${props.data}`} />
-);
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.src = `data:${props.mediatype};base64,${props.data}`;
+    }
+  }, [props.data]);
+
+  return (
+    <div className="cell-output-plot-background">
+      <img ref={ref} alt="" />
+    </div>
+  );
+};
+
 MediaJPG.defaultProps = {
   mediaType: "image/jpeg",
 };
 
-export const MediaGIF = (props: any) => (
-  <img alt="" src={`data:${props.mediatype};base64,${props.data}`} />
-);
+export const MediaGIF = (props: any) => {
+  const ref = React.useRef<HTMLImageElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.src = `data:${props.mediatype};base64,${props.data}`;
+    }
+  }, [props.data]);
+
+  return (
+    <div className="cell-output-plot-background">
+      <img ref={ref} alt="" />
+    </div>
+  );
+};
+
 MediaGIF.defaultProps = {
   mediaType: "image/gif",
 };
