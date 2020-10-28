8db2976f1e43fea29db89938478ff2e833ce94be
Bao Nguyen <b@nqbao.com>
2020-10-28 12:23:28 -0700

    start api on start up

--- docker/ide/app.sh
+++ docker/ide/app.sh
@@ -25,11 +25,21 @@ fi
     node /home/theia/browser-app/src-gen/backend/main.js "$WORKSPACE_PATH/$WORKBENCH_NAME" --hostname=0.0.0.0 --port 3001 &
 )
 
-(
+
+start-jupyter() {
     cd $WORKSPACE_PATH
     EXTRAPATH=$(find `pwd`/ -maxdepth 1 -type d | xargs -n 999 | tr -s ' ' ':')
     export PYTHONPATH=$EXTRAPATH:$PYTHONPATH
-    jupyter-notebook --allow-root &
-)
+    jupyter-notebook --allow-root
+}
+
+start-api() {
+    DIR_NAME=`python -c "import h1st.cli.project as prj; print(prj._clean_name('$WORKBENCH_NAME')[0])"`
+    cd $WORKSPACE_PATH/$DIR_NAME
+    uvicorn --port 3002 h1st.contrib.tune_server.server:app
+}
+
+start-jupyter &
+start-api &
 
 exec traefik --configfile /opt/traefik.yml
