6ee4f6044b6685641c4f4d1f97d945f76c4bb306
Khoa Ma <khoama@gmail.com>
2020-12-22 18:28:50 -0800

    — fix auth token

--- services/web/src/app.tsx
+++ services/web/src/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
+import Cookies from 'js-cookie';
 import Composer from 'containers/composer';
+import { authActions } from 'reducers/auth';
 import keycloak from './auth';
+
+import store from 'stores';
 
 import 'app.css';
 
@@ -9,13 +13,19 @@ export function LoadingCheck() {
   return <p>Loading...</p>;
 }
 
+function setAuthInfo(token: string | undefined) {
+  const dispatch = store.dispatch;
+  dispatch(authActions.setToken({ token }));
+  Cookies.set('token', token || '', { path: '/' });
+}
+
 export default function App() {
   const onEvent = (event: any, data: any) => {
     console.log('On event', event, data);
   };
 
-  const onTokens = (token: any) => {
-    alert(token);
+  const onTokens = (data: any) => {
+    setAuthInfo(data.token);
   };
   return (
     <ReactKeycloakProvider
