1fa1fe1675aefbdd5da353d48a99c8af909f35fe
Hung Vo <vphungkhtn@gmail.com>
2021-01-08 13:18:20 +0700

    Serving form

--- ide/h1st/src/browser/packages/serving/components/servingContent.tsx
+++ ide/h1st/src/browser/packages/serving/components/servingContent.tsx
@@ -1,5 +1,57 @@
 import * as React from 'react';
+import { SelectBox } from './widget/SelectBox';
+
+const SelectGraphClass = () => {
+  return (
+    <div className="form-section">
+      <div className="section-title">Class</div>
+      <div>
+        <select className="serving-input-select">
+          <option className="form-label">graph class</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+const InstanceSize = () => {
+  return (
+    <div className="form-section">
+      <div className="section-title">Instance Size</div>
+      <div className="row space-between">
+        <div className="form-label">Auto-scaling</div>
+        <div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+      </div>
+      <div>
+        <SelectBox />
+      </div>
+      <div>
+        <button className="deploy-button" type="button">
+          Deploy
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const DeployForm = () => {
+  return (
+    <div className="serving-content serving-left">
+      <SelectGraphClass />
+      <InstanceSize />
+    </div>
+  );
+};
 
 export const ServingContent = () => {
-  return <div>Serving content</div>;
+  return (
+    <div style={{ height: '100%' }}>
+      <DeployForm />
+    </div>
+  );
 };
---
+++ ide/h1st/src/browser/packages/serving/components/widget/SelectBox/index.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import classnames from 'classnames';
+
+interface IRadioInputItem {
+  value: string;
+  name: string;
+  title: string;
+  description?: string | JSX.Element;
+  onSelect: Function;
+  selected: any;
+}
+
+const InputItem = ({
+  value = '',
+  name = '',
+  title,
+  description,
+  onSelect,
+  selected,
+}: IRadioInputItem) => {
+  const checkboxId = `checkbox-${value}`;
+  return (
+    <div
+      className={classnames('radio-button-item', {
+        selected: selected === value,
+      })}
+    >
+      <div className="input-box">
+        <input
+          id={checkboxId}
+          type="radio"
+          value={value}
+          name={name}
+          onChange={e => onSelect(e.target.value)}
+        />
+      </div>
+      <div className="information">
+        <label htmlFor={checkboxId}>
+          <div className="title">{title}</div>
+          <div className="description">{description}</div>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+const SelectBox = ({ name = 'select' }) => {
+  const [selected, setSelected] = React.useState();
+  const choices = [
+    {
+      name,
+      value: 'large',
+      title: 'Large',
+      description: <div>8GB RAM | 4 CPU</div>,
+    },
+    {
+      name,
+      value: 'medium',
+      title: 'Medium',
+      description: <div>4GB RAM | 2 CPU</div>,
+    },
+    {
+      name,
+      value: 'small',
+      title: 'Small',
+      description: <div>2GB RAM | 1 CPU</div>,
+    },
+  ];
+  return (
+    <div className="radio-button-box">
+      {choices.map((choice, idx) => (
+        <InputItem
+          key={idx}
+          {...choice}
+          selected={selected}
+          onSelect={setSelected}
+        />
+      ))}
+    </div>
+  );
+};
+
+export { SelectBox };
