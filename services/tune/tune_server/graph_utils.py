e724706aabf808a3658c2eb8993fcc2c0f902167
Thang Tran <thangtp@gmail.com>
2021-01-08 17:10:21 -0800

    fix topology attr

--- services/tune/tune_server/graph_utils.py
+++ services/tune/tune_server/graph_utils.py
@@ -103,13 +103,13 @@ def get_graph_topology(graph_name):
                     else ('action'
                         if id not in ['start', 'end']
                         else None),
+            'ensemble_sub_models':
+                [m.__class__.__name__ for m in node._containable._sub_models]
+                    if isinstance(node._containable, Ensemble)
+                    else None,
             'edges': [{
                 'next_node_id': edge[0].id,
-                'edge_label': edge[1],
-                'ensemble_sub_models':
-                    [m.__class__.__name__ for m in node._containable._sub_models]
-                        if isinstance(node._containable, Ensemble)
-                        else None
+                'edge_label': edge[1]              
             } for edge in node.edges]   
         }
 
