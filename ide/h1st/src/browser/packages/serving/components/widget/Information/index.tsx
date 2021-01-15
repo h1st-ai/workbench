c34d38d1d47450e0af1af62059cf12640af729f9
Hung Vo <vphungkhtn@gmail.com>
2021-01-15 19:31:58 +0700

    Serving table

--- ide/h1st/src/browser/packages/serving/components/widget/Information/index.tsx
+++ ide/h1st/src/browser/packages/serving/components/widget/Information/index.tsx
@@ -1,4 +1,33 @@
 import * as React from 'react';
+import { CopyIcon } from './Icons/Icons';
+
+const ServingItem = () => {
+  return (
+    <tr className="deployment-row">
+      <td>serve_1</td>
+      <td>graphClassName</td>
+      <td>David Gil</td>
+      <td>Dec 20, 2020</td>
+      <td>Large</td>
+      <td>Active</td>
+      <td>
+        <span className="button-group">
+          <button className="serving-table-button serving-table-button__start">
+            Start
+          </button>
+          <button className="serving-table-button serving-table-button__stop">
+            Stop
+          </button>
+          <button className="serving-table-button serving-table-button__url">
+            <CopyIcon />
+            URL
+          </button>
+          <button className="serving-table-button serving-table-button__remove"></button>
+        </span>
+      </td>
+    </tr>
+  );
+};
 
 const DeploymentHistoryTable = () => {
   return (
@@ -8,54 +37,26 @@ const DeploymentHistoryTable = () => {
           <tr>
             <th>Name</th>
             <th>Graph</th>
-            <th>Status</th>
+            <th>Deployed by</th>
             <th>Deployed On</th>
-            <th>Instance Size</th>
+            <th>Instances</th>
+            <th>Status</th>
+            <th style={{ width: '10%' }}></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>serve_1</td>
-            <td>graphClassName</td>
-            <td>Active</td>
-            <td>Dec 20, 2020</td>
-            <td>Large</td>
-          </tr>
-          <tr>
-            <td>serve_1</td>
-            <td>graphClassName</td>
-            <td>Active</td>
-            <td>Dec 20, 2020</td>
-            <td>Large</td>
-          </tr>
-          <tr>
-            <td>serve_1</td>
-            <td>graphClassName</td>
-            <td>Active</td>
-            <td>Dec 20, 2020</td>
-            <td>Large</td>
-          </tr>
-          <tr>
-            <td>serve_1</td>
-            <td>graphClassName</td>
-            <td>Active</td>
-            <td>Dec 20, 2020</td>
-            <td>Large</td>
-          </tr>
-          <tr>
-            <td>serve_1</td>
-            <td>graphClassName</td>
-            <td>Active</td>
-            <td>Dec 20, 2020</td>
-            <td>Large</td>
-          </tr>
+          {Array(6)
+            .fill({})
+            .map((_, idx) => (
+              <ServingItem key={idx} />
+            ))}
         </tbody>
       </table>
     </div>
   );
 };
 
-const DeploymentMonitoringItem = props => {
+const DeploymentMonitoringItem = (props: any) => {
   const { title = 'Total predictions', number = 10 } = props;
   return (
     <div className="item">
