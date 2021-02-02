25ad9d0af4b9ff798e2ab7a646afb15993865653
Hung Vo <vphungkhtn@gmail.com>
2021-02-02 11:12:10 +0700

    Refactor - Change tune server enpoint

--- ide/h1st/src/node/config.ts
+++ ide/h1st/src/node/config.ts
@@ -1,4 +1,4 @@
 export default {
   AUTH_HOST: 'https://login.h1st.ai/auth',
-  TUNE_HOST: process.env.TUNE_URL || 'https://staging.h1st.ai/tune',
+  TUNE_HOST: process.env.TUNE_URL || 'http://localhost:3000',
 };
