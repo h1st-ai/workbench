03a64c89b58291e733cfbfd13dd50e7704f12083
Hung Vo <vphungkhtn@gmail.com>
2021-02-08 17:36:30 +0700

    [Serving Panel] - expose serving path

--- ide/h1st/src/node/h1st-backend-with-client-service.ts
+++ ide/h1st/src/node/h1st-backend-with-client-service.ts
@@ -283,14 +283,13 @@ export class H1stBackendWithClientServiceImpl
   }
 
   async cloneTemplateRepo(name: string) {
-    await execCommand(`rm -rf ${process.env.WORKSPACE_PATH}/temp`);
+    const path = Settings.WORKSPACE_PATH;
+    await execCommand(`rm -rf ${path}/temp`);
     await execCommand(
-      `git clone https://github.com/h1st-ai/H1st-AI-App-Templates.git ${process.env.WORKSPACE_PATH}/temp`,
+      `git clone https://github.com/h1st-ai/H1st-AI-App-Templates.git ${path}/temp`,
     );
 
-    await execCommand(
-      `mv ${process.env.WORKSPACE_PATH}/temp/${name} ${process.env.WORKSPACE_PATH}/${name}`,
-    );
-    await execCommand(`rm -rf ${process.env.WORKSPACE_PATH}/temp`);
+    await execCommand(`mv ${path}/temp/${name} ${path}/${name}`);
+    await execCommand(`rm -rf ${path}/temp`);
   }
 }
