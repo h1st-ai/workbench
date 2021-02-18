e6ba55c73db8d137e777fca8352125c0c303c711
Vu Nhan <nhanitvn@gmail.com>
2021-02-17 16:29:47 -0800

    Refactor the entrypoint.sh a bit

--- docker/ide/entrypoint.sh
+++ docker/ide/entrypoint.sh
@@ -4,18 +4,21 @@
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+PYTHON_REQ_FILE=/home/project/_requirements.txt
+PYTHON_REQ_START_FILE=/home/project/_requirements_start.txt
+PYTHON_REQ_STOP_FILE=/home/project/_requirements_stop.txt
+
 #Define cleanup procedure
 preStop() {
     echo "Container stopped, performing cleanup..."
-    pip3 freeze > /home/project/requirements_xyz_stop.txt
+    pip3 freeze > $PYTHON_REQ_STOP_FILE
     # Check for difference and keep only new packages
-    awk 'NR == FNR{ a[$0] = 1;next } !a[$0]'  /home/project/requirements_xyz_start.txt /home/project/requirements_xyz_stop.txt > /home/project/requirements_xyz.txt
-    rm /home/project/requirements_xyz_start.txt
-    rm /home/project/requirements_xyz_stop.txt
+    awk 'NR == FNR{ a[$0] = 1;next } !a[$0]'  $PYTHON_REQ_START_FILE $PYTHON_REQ_STOP_FILE > $PYTHON_REQ_FILE
+    rm $PYTHON_REQ_START_FILE
+    rm $PYTHON_REQ_STOP_FILE
 }
 
-pip3 freeze > /home/project/requirements_xyz_start.txt
-PYTHON_REQ_FILE=/home/project/requirements_xyz.txt
+pip3 freeze > $PYTHON_REQ_START_FILE
 if [ -f "$PYTHON_REQ_FILE" ]; then
     # Install new packages
     echo "Install installed packages from the last session"
