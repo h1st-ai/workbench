fa7791b1655f1b6fca2acbf53c3d9fa4dddda280
Tuan Pham <phamhoangtuan.it@gmail.com>
2021-05-02 09:39:59 +0700

    Command to run docker with GPU

--- docker/ide/dev-docker.sh
+++ docker/ide/dev-docker.sh
@@ -4,3 +4,6 @@
 apt-get install nvidia-container-runtime
 
 docker run -it --init -p 3000:3000  -v "$(pwd)/browser-app:/home/theia/browser-app" -v "$(pwd)/h1st:/home/theia/h1st"  -e WORKSPACE_PATH="/home/project"  -e WORKBENCH_NAME="H1stSample"  h1st/workbench:latest
+
+# run docker with --gpus flag and cuda lib from GPU machine
+docker run -d -p 80:3000 -v "/home/ubuntu/data:/home/project" -v "/usr/local/cuda-11.0:/usr/local/cuda-11.0" --name workbench_gpus --restart=always --gpus all workbench
