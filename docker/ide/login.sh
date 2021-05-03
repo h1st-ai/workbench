994d0393f8005d1b437a9a96997fdfa69eca43c3
Tuan Pham <phamhoangtuan.it@gmail.com>
2021-05-03 09:25:34 +0700

    Parameterize ECR repo link in shell script

--- docker/ide/login.sh
+++ docker/ide/login.sh
@@ -1,2 +1,3 @@
 export $(cat .env | xargs)
-aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 394497726199.dkr.ecr.us-west-1.amazonaws.com
+ECR_REPO_LINK="733647232589.dkr.ecr.us-west-1.amazonaws.com"
+aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin $ECR_REPO_LINK
